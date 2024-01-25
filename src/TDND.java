ff481020eabf652ec3614785c4d74725dcfa0f79
Matti Rundelin MacBook <matti.rundelin@gmail.com>
2024-01-25 22:26:11 +0200

    IntelliJ had some decent code autofixes. Tried and committed them

--- src/TDND.java
+++ src/TDND.java
@@ -15,14 +15,13 @@ public class TDND {
 
     public static int diceRoller(int howMany, int max){
         int finalRoll = 0;
-        int currentRoll = 0;
+        int currentRoll;
         
         
         for (int i = 0; i < howMany; i++) {
             currentRoll = ThreadLocalRandom.current().nextInt(1, max + 1);
             finalRoll = finalRoll + currentRoll;
             System.out.println("roll " + (i + 1) + ": "+ currentRoll);
-            currentRoll = 0;
         }
     
         
@@ -31,15 +30,14 @@ public class TDND {
     
     public static void main(String[] args) {
         Scanner UserInput = new Scanner(System.in);
-        int roll = 0;
 
         System.out.print("Enter dice amount, and dice type (format '1d6') :");
         String userInput = UserInput.nextLine();
         
         
-        String pieces[] = userInput.split("d");
-        int diceAmount = Integer.valueOf(pieces[0]);
-        int diceType = Integer.valueOf(pieces[1]);
+        String[] pieces = userInput.split("d");
+        int diceAmount = Integer.parseInt(pieces[0]);
+        int diceType = Integer.parseInt(pieces[1]);
         
         System.out.println(diceAmount + " " + diceType);
         
