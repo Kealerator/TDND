ef0657f23f96d99e7d37c1f5a22d915b82697d9d
Matti Rundelin <matti.rundelin@gmail.com>
2024-01-28 16:37:59 +0200

    lol inputValidator still needed some work.
    Now user is able to input anything and the validator will validate if the input syntax is passable.
    
    Only way I was able to crash was when I typed simply 'd6'.
    
    For now, I think it's fine to move merge it to beta.

--- src/TDND.java
+++ src/TDND.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.regex.Pattern;
 
 
 public class TDND {
@@ -50,7 +51,7 @@ public class TDND {
             diceType = Integer.parseInt(pieces[1]);             //1 is the type of dice (4,6,8,20,12,20,100)
             bonusVariable = Integer.parseInt(pieces[2]);        //2 is the bonus amount
             bonusType = "+";
-        } else if (userInput.contains("-")) {                   // the input has "-" so we will substract the bonus
+        } else if (userInput.contains("-")) {                   // the input has "-" so we will subtract the bonus
             String[] pieces = userInput.split("[d-]");   // split the bonus, and the dice amount & type
             diceAmount = Integer.parseInt(pieces[0]);          // set the input values to int variables so
             diceType = Integer.parseInt(pieces[1]);            // you can just throw the values to function
@@ -67,6 +68,18 @@ public class TDND {
     }
 
     public static boolean inputValidator(String input) {
+
+        /*
+                Trail of thought:
+                    1. Check if there is a number before and after 'd'
+                    2. If there is a number before and after 'd', check how many 'd', '+' and '-' there are
+                    3. If there is a number before and after, and there is no extra 'd','+' or '-', check if there
+                        is a die type after the 'd'.
+                    4. Check if the die type is correct from int Array list of approved die sizes.
+
+         */
+
+
         boolean valid = true;
         char dice = 'd';
         char plus = '+';
@@ -76,35 +89,49 @@ public class TDND {
         int mCount = 0;
 
 
-        //First, check if there is too many characters d, +, or -.
+        // First, scan user input string if there is any numbers before and after 'd'
+            if (input.contains("d")) {
+                String[] pieces = input.split("d");
+                if (Pattern.matches("[a-zA-Z]+", pieces[0])) {
+                    valid = false;
+                }
+                if (pieces.length > 1){
+                    if (Pattern.matches("[a-zA-Z]+", pieces[1])) {
+                        valid = false;
+                    }
+            }
+        }
+
+        // Second, check if there is too many characters d, +, or -.
         // d searcher
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == dice) {
-                dCount++;
+        if (valid) {
+            for (int i = 0; i < input.length(); i++) {
+                if (input.charAt(i) == dice) {
+                    dCount++;
+                }
             }
-        }
-        // + searcher
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == plus) {
-                pCount++;
+            // + searcher
+            for (int i = 0; i < input.length(); i++) {
+                if (input.charAt(i) == plus) {
+                    pCount++;
+                }
             }
-        }
-        // - searcher
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == minus) {
-                mCount++;
+            // - searcher
+            for (int i = 0; i < input.length(); i++) {
+                if (input.charAt(i) == minus) {
+                    mCount++;
+                }
+            }
+
+            if (dCount > 1 || pCount > 1 || mCount > 1) {
+                valid = false;
+            }
+            if (dCount == 0 && pCount == 0 && mCount == 0) {
+                valid = false;
             }
         }
 
-        if (dCount > 1 || pCount > 1 || mCount > 1) {
-            valid = false;
-        }
-        if (dCount == 0 || pCount == 0 || mCount == 0) {
-            valid = false;
-        }
-
-
-        // Second, we need to check if there even is a die type
+        // Third, we need to check if there even is a die type
         // until now if the valid is false, there is no reason to check this.
         if (valid){
 
@@ -128,7 +155,7 @@ public class TDND {
         }
 
 
-        //Third, check if the dice type is correct (d4, d6, d10, d12, d20, d100)
+        //Fourth, check if the dice type is correct (d4, d6, d10, d12, d20, d100)
         //until now if the valid is false, there is no reason to check this.
 
         if(valid){
--- src/testing.java
+++ src/testing.java
@@ -1,7 +1,20 @@
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class testing {
 
     public static void main(String[] args) {
+        Scanner UserInput = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String userInput = UserInput.nextLine();
+
+        String text = "abc";
+        String number;
+
+        if (!Pattern.matches("[a-zA-Z]+", userInput)) {
+            System.out.println("You've entered a number.");
+        }
+
     }
 }
