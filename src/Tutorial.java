3b2356c166441ec0ef31086cfd61adf5c3f7f0fe
MattiR-MBPUbu <matti.rundelin@gmail.com>
2024-03-20 09:56:08 +0200

    going in brave, creating a new file & a class for tutorial.

---
+++ src/Tutorial.java
@@ -0,0 +1,14 @@
+
+public class Tutorial {
+    public Tutorial(){
+
+    }
+    
+    public void start(){
+
+    }
+
+    private void clearTerminal(){
+        System.out.print("\033[H\033[2J");
+    }
+}
